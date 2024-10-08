0edb16cf512b6b6a84e4a3ddac3e51f8c41bbd09
ryanjk02 <ryan@symcar.com>
2024-10-08 16:18:21 +0100

    sdasd

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,18 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
 import Login from './components/Login';
+import Signup from './components/Signup';  // Import Signup component
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar';  // Adding a navigation bar
 
 const App = () => {
   return (
     <Router>
+      <Navbar />  {/* Adding a navbar */}
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />  {/* Add the signup route */}
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </Router>
   );
 };
+
 export default App;
