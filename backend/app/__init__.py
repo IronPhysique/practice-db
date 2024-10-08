143365e7072248eb45181533adc2bc38501c8cbd
ryanjk02 <ryan@symcar.com>
2024-10-08 11:16:47 +0100

    better

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-import os
 from dotenv import load_dotenv
+import os
+from app.routes import routes  # Ensure correct path to routes
 
 # Load environment variables from .env file
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    
+    # Register blueprint routes
+    app.register_blueprint(routes)
 
     # Configure the JWT secret key from environment variables
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
