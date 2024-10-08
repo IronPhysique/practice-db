43456f2a4643011aacdb7b083e276740a8c6d28d
ryanjk02 <ryan@symcar.com>
2024-10-08 11:45:00 +0100

    s

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
-
-
-<Route path="/" element={<Login />} />
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        {/* Protect the Dashboard route */}
         <Route
           path="/dashboard"
           element={
