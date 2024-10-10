cb1c8f38caab895daf6411e96049334b26acb055
ryanjk02 <ryan@symcar.com>
2024-10-10 11:34:56 +0100

    imports

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,6 +5,8 @@ import Signup from './components/pages/Signup';  // Import Signup component
 import Dashboard from './components/pages/Dashboard';
 import PrivateRoute from './components/routes/PrivateRoute';
 import Navbar from './components/pages/Navbar';  // Adding a navigation bar
+import StarterForm from './components/forms/StarterForm';
+import LeaverForm from './components/forms/LeaverForm';
 
 const App = () => {
   return (
